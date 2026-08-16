d89f901db21f6745eddb6afb004b8e7cb4cf0ac9
Censera <Censera@proton.me>
2026-08-16 21:10:51 +0100

    remodel: remove Git-state commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,11 +1,5 @@
-//! Each command decides what should happen, it delegates
-//! how to the `git` module and is something wrong to `checks`.
-
-pub mod check;
-pub mod repair;
 pub mod save;
 pub mod start;
 pub mod status;
 pub mod sync;
 pub mod undo;
-pub mod unsync;
